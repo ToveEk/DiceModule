9669852654b0db8d2b5dcc215899be93ce683c6f
Tove Ek <te222uu@student.lnu.se>
2025-11-06 11:31:54 +0100

    Add section headers to test output for better readability

--- test/testApp.js
+++ test/testApp.js
@@ -15,6 +15,7 @@ const diceRoller = new Dice()
 // Basic rolls
 // ---------------
 
+console.log('==== Basic Rolls ====')
 console.log(diceRoller.startRolling('2d6+1')) // Roll two six-sided dice with plus 1 modifier
 
 console.log(diceRoller.startRolling('d4+5')) // Roll a single four-sided die with plus 5 modifier
@@ -25,6 +26,7 @@ console.log(diceRoller.startRolling('3d8-2')) // Roll three eight-sided dice wit
 // Rolls with single dice types
 // -------------------------------
 
+console.log('\n==== Rolls with Single Dice Types ====')
 console.log(diceRoller.startRolling('d10')) // Roll a single ten-sided die
 console.log(diceRoller.startRolling('d12')) // Roll a single twelve-sided die
 console.log(diceRoller.startRolling('d100')) // Roll a single one-hundred-sided die
@@ -33,6 +35,7 @@ console.log(diceRoller.startRolling('d100')) // Roll a single one-hundred-sided 
 // Rolls with advantage/disadvantage
 // ------------------------------------
 
+console.log('\n==== Rolls with Advantage/Disadvantage ====')
 console.log(diceRoller.startRolling('2d20 with advantage')) // Roll two twenty-sided dice with advantage
 console.log(diceRoller.startRolling('3d20 with disadvantage')) // Roll two twenty-sided dice with disadvantage
 
@@ -40,22 +43,25 @@ console.log(diceRoller.startRolling('3d20 with disadvantage')) // Roll two twent
 // Natural 20 and 1
 // -----------------
 
+console.log('\n==== Natural 20 and Natural 1 ====')
 console.log(diceRoller.startRolling('d20')) // Roll a single twenty-sided die (may result in natural 20 or 1)
 
 // -----------------
 // Invalid rolls
 // -----------------
 
-console.log(diceRoller.startRolling('d5')) // Invalid die type
-console.log(diceRoller.startRolling('6')) // Invalid notation
-console.log(diceRoller.startRolling()) // Invalid notation
-console.log(diceRoller.startRolling('2d20 with advntage')) // Misspelled advantage
-console.log(diceRoller.startRolling('2d4 with advantage')) // Advantage not allowed with d4
+console.log('\n==== Invalid Rolls ====')
+// console.log(diceRoller.startRolling('d5')) // Invalid die type
+// console.log(diceRoller.startRolling('6')) // Invalid notation
+// console.log(diceRoller.startRolling()) // Invalid notation
+// console.log(diceRoller.startRolling('2d20 with advntage')) // Misspelled advantage
+// console.log(diceRoller.startRolling('2d4 with advantage')) // Advantage not allowed with d4
 
 // -----------------
 // Show history
 // -----------------
-console.log('Showing history of previous rolls:')
+
+console.log('\n==== Show Roll History ====')
 diceRoller.showHistory(100) // Show the last 100 rolls
 diceRoller.showHistory(3) // Show the last 3 rolls
 diceRoller.showHistory(0) // Invalid input
